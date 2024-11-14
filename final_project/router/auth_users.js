0b800d971c8a04eb0e07ed45650968180a9d3e97
Chrissie <kanjoc@gmail.com>
2024-11-14 02:17:47 -0500

    task 14

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -75,6 +75,19 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
   else {
       return res.status(404).json({message: `ISBN ${isbn} not found`});
   }});
+  
+  regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    const username = req.session.authorization.username;
+    if (books[isbn]) {
+      let book = books[isbn];
+      delete book.reviews[username];
+      return res.status(200).send("Review successfully deleted");
+    }
+    else {
+      return res.status(404).json({message: `ISBN ${isbn} not found`});
+    }
+  });
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -17,27 +17,58 @@ public_users.post("/register", (req,res) => {
 });
 
 
-//  Get the book list available in the shop
+// Get book lists
+const getBooks = () => {
+    return new Promise((resolve, reject) => {
+        resolve(books);
+    });
+};
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    //Write your code here
-    res.send(JSON.stringify(books, null, 4));
+public_users.get('/',async function (req, res) {
+    try {
+      const bookList = await getBooks(); 
+      res.json(bookList); // Neatly format JSON output
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Error retrieving book list" });
+    }
   });
 
 
 // Get book details based on ISBN
 //  Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+/*public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
     if (books[isbn]) {
         return res.json(books[isbn]);
     } else {
         return res.status(404).json({ message: "Book not found" });
     }
+ });*/
+ // Get book details based on ISBN
+const getByISBN = (isbn) => {
+    return new Promise((resolve, reject) => {
+        let isbnNum = parseInt(isbn);
+        if (books[isbnNum]) {
+            resolve(books[isbnNum]);
+        } else {
+            reject({ status: 404, message: `ISBN ${isbn} not found` });
+        }
+    });
+};
+
+
+//  Get book details based on ISBN
+public_users.get('/isbn/:isbn',function (req, res) {
+    getByISBN(req.params.isbn)
+    .then(
+        result => res.send(result),
+        error => res.status(error.status).json({message: error.message})
+    );
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+/*public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;  
   const authorBooks = [];  
   
@@ -52,10 +83,17 @@ public_users.get('/author/:author',function (req, res) {
   } else {
     res.status(404).send('No books found for author');  
   }
+});*/
+public_users.get('/author/:author',function (req, res) {
+    const author = req.params.author;
+    getBooks()
+    .then((bookEntries) => Object.values(bookEntries))
+    .then((books) => books.filter((book) => book.author === author))
+    .then((filteredBooks) => res.send(filteredBooks));
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
+/*public_users.get('/title/:title',function (req, res) {
     const title = req.params.title.toLowerCase();
     const filteredBooks = Object.values(books).filter(book => book.title.toLowerCase().includes(title));
     if(filteredBooks.length > 0){
@@ -64,6 +102,13 @@ public_users.get('/title/:title',function (req, res) {
     else{
         return res.status(404).json({message: "Book not found"});
     }
+});*/
+public_users.get('/title/:title',function (req, res) {
+    const title = req.params.title;
+    getBooks()
+    .then((bookEntries) => Object.values(bookEntries))
+    .then((books) => books.filter((book) => book.title === title))
+    .then((filteredBooks) => res.send(filteredBooks));
 });
 
 //  Get book review
@@ -75,5 +120,6 @@ public_users.get('/review/:isbn',function (req, res) {
     const reviews = books[isbn].reviews;
     return res.status(200).json({ reviews: reviews });
 });
+
 
 module.exports.general = public_users;
